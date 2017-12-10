478f9020edd905dd4aca21dbbc9c028734a54a1d
Enr1g <enr1g@outlook.com>
2017-12-10 19:59:51 +0300

    Closing urlopen

--- handlers.py
+++ handlers.py
@@ -14,13 +14,12 @@ Usage:
 
 def get_an_owl(bot, chat_id):
     owl = get_owl_url()
-    owl = urlopen(owl)
-    
-    bot.send_photo(chat_id, owl.read(), "Get your random owl! ;)")
+    with urlopen(owl) as f:
+        bot.send_photo(chat_id, f.read(), "Get your random owl! ;)")
 
 
 def print_about(bot, chat_id):
-    bot.send_message(chat_id, """The bot is designed to send a random owl to my favourite one :3""")
+    bot.send_message(chat_id, """The bot is designed to send a random owl to my favourite one :3 Yep, Xenia, I'm talking to you ;)""")
 
 handlers = {
     "/get": get_an_owl,
